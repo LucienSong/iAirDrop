813d1845bbefcea04f43dddd97901667b358b992
Lucien Song <LucienSong@gmx.com>
2018-12-26 18:47:18 +0800

    Add blacklist and droped recording
    Add blacklist and droped recording

--- iAirDrop.py
+++ iAirDrop.py
@@ -19,6 +19,17 @@ memo = '\" blablabla \"'
 walletName = "wallet"
 walletPass = "PW5-------walletPass--------GA"
 
+# set files
+accounts = "accounts.csv"
+blacklist = "blacklist.csv"
+droped = "droped.csv"
+
+def write(content,file):
+    file_name = file
+    f = open(file_name,'a')
+    write = csv.writer(f)
+    write.writerow(content)
+    f.close()
 
 def send(accout):
     os.system("cleos -u http://mainnet.eoscalgary.io --wallet-url=http://127.0.0.1:8900 push action " + token + " transfer '[ " + sender + " , \"" + accout + "\" , " + asset + " , " + memo + " ]' -p " + sender)
@@ -30,12 +41,34 @@ def logPrint(msg):
 if __name__ == '__main__':
     logPrint("start airdrop...")
     os.system("cleos  -u http://mainnet.eoscalgary.io --wallet-url=http://127.0.0.1:8900 wallet unlock -n " + walletName + " --password " + walletPass)
-    f = open("accounts.csv")
-    # f = open("drop.csv")
-    rows = csv.reader(f)
-    for row in rows:
+    
+    f = open(accounts)
+    b = open(blacklist)
+
+    accRows = csv.reader(f)
+    blkRows = csv.reader(b)
+
+    for row in accRows:
+        new = True
+        num += 1
+
+        # if you need random amout
+        # rand = random.randint(0,100)+100
+        # asset = '\"' + str(rand) + '.0000 HIG\"'
+
         to = row[0]
-        send(to)
-        logPrint(to)
+
+        for blk in blkRows:
+            if blk[0] == to:
+                new = False 
+                break
+        
+        if new:
+            write(row, droped)
+            send(to)
+            logPrint(to + ' # ' + str(num))
+        else:
+            logPrint(to + ' # ' + str(num) + '***IN BLACKLIST***')
+
         time.sleep(interval)
     logPrint("airdrop done...")
